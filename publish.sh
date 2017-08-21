5d6195cd8901feb400ae9e2b9ed9d4b0473692c6
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-08-20 21:52:28 -0700

    Add back eslint plugin import to package.json

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 rm index.zip
 zip -r -X ./index.zip *
-aws lambda update-function-code --function-name 'myqGarage' --region us-east-1 --zip-file 'fileb://index.zip'
+aws lambda update-function-code --function-name 'myqGarage' --region eu-west-1 --zip-file 'fileb://index.zip'
